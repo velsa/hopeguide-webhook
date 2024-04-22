13eac30b75e5b658b19dd5672938fe718e3f72d3
velsa <velshome@yahoo.com>
2024-04-22 17:36:58 +0300

    log

--- src/handlers/handle-relevant-until.ts
+++ src/handlers/handle-relevant-until.ts
@@ -5,21 +5,24 @@ import { g } from '../utils/g'
 export async function handleRelevantUntil(data: VacanciesResponse) {
   const db = new VacanciesDatabase({ notionSecret: g.env.NOTION_API_SECRET })
   const props = new VacanciesResponseDTO(data).properties
+  const relevantUntil = new Date(props.lastEditedTime)
 
-  console.log(`${data.id} handleRelevantUntil:`, props.relevantUntil.start)
+  relevantUntil.setDate(relevantUntil.getDate() + 30)
 
-  if (new Date(props.relevantUntil.start).getTime() < Date.now() + 1000 * 60 * 60 * 24 * 30) {
-    console.log(`${data.id} handleRelevantUntil: relevant until date ⊕ 30 days`, props.relevantUntil.start)
+  console.log(
+    `${data.id} handleRelevantUntil: change ${props.relevantUntil.start} to ${formatISO(relevantUntil, {
+      representation: 'date',
+    })}`,
+  )
 
-    const patch = new VacanciesPatchDTO({
-      properties: {
-        relevantUntil: {
-          start: formatISO(new Date(Date.now() + 1000 * 60 * 60 * 24 * 30), { representation: 'date' }),
-        },
+  const patch = new VacanciesPatchDTO({
+    properties: {
+      relevantUntil: {
+        start: formatISO(relevantUntil, { representation: 'date' }),
       },
-    })
+    },
+  })
 
-    await db.updatePage(data.id, patch)
-    g.pageLog.info(data.id, `updated: Актуально до`)
-  }
+  await db.updatePage(data.id, patch)
+  g.pageLog.info(data.id, `updated: Актуально до`)
 }
